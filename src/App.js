d6da54234ba578ce76d7c4a625c74863c25e556d
cha <zizi2481@gmail.com>
2022-12-12 20:05:01 +0900

    second

--- src/App.js
+++ src/App.js
@@ -7,37 +7,61 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Notes from "./components/Notes";
 import { Col, Container, Row } from "react-bootstrap";
+import GoogleLogin from "./components/google";
+// import KakaoLogin from "./components/KaKaoLogin";
+// import { gapi } from "gapi-script";
 
 function App() {
   const showUpdateNote = useSelector((state) => state.note.showUpdateNote);
 
   return (
-    <>
-      <header>Doodle</header>
-      <div className="App">
-        <Container>
-          <Row>
-            <Col xs={12} md={6}>
-              <Calendar />
-            </Col>
-            <Col xs={12} md={6}>
-              <Notes />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={6}>
-              <AddNote />
-            </Col>
-            <Col xs={12} md={6}>
-              {showUpdateNote && <UpdateNote />}
-            </Col>
-          </Row>
-        </Container>
-      </div>
-      <footer>
-        Copyright &copy; 2020 by Sumit Jangir. All Rights Reserved.
-      </footer>
-    </>
+    <body>
+      <>
+        <header>
+          <h1>MENU</h1>
+          <nav>
+            <span>Menu1</span>
+            <span>Menu2</span>
+            <span>Menu3</span>
+            <span className="aApp">
+              <GoogleLogin
+                success={(res) => console.log(res)}
+                fail={(res) => console.log(res)}
+              />
+            </span>
+          </nav>
+        </header>
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className="">
+          <Container>
+            <Row>
+              <Col xs={40} md={50}>
+                <Calendar />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={6}>
+                <Notes />
+              </Col>
+              <Col xs={12} md={6}>
+                <AddNote />
+              </Col>
+              <Col xs={12} md={6}>
+                {showUpdateNote && <UpdateNote />}
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <br></br>
+        <br></br>
+        <br></br>
+        <footer>
+          Copyright &copy; 2020 by Sumit Jangir. All Rights Reserved.
+        </footer>
+      </>
+    </body>
   );
 }
 
